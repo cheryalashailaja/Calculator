0c71e946487c7a9e6f9966d01d6db9dbc296a0f0
iStar <iStar@DESKTOP-BR1289H>
2017-05-25 09:02:05 +0530

    This is assignment opertor

---
+++ src/com/onlinecalculator/calculator/calctypes/assignment.java
@@ -0,0 +1,24 @@
+package com.onlinecalculator.calculator.calctypes;
+
+import java.util.Scanner;
+
+public class assignment
+{
+public static void main(String args[])
+{
+	int a,b;
+	Scanner s=new Scanner(System.in);
+	System.out.println("enter first number");
+	a=s.nextInt();
+	System.out.println("enter second number");
+	b=s.nextInt();
+	System.out.println("a+=b"+(a+=b));
+	System.out.println("a-=b"+(a-=b));
+	System.out.println("a*=b"+(a*=b));
+	System.out.println("a/=b"+(a/=b));
+	System.out.println("modules"+(a%=b));
+	
+	
+
+}
+}
