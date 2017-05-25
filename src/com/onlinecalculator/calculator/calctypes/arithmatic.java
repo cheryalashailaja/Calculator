7518106dc11efad7cbdabd9c54d3347f6156fcf4
iStar <iStar@DESKTOP-BR1289H>
2017-05-25 08:57:17 +0530

    This is all arithmatic opertion

---
+++ src/com/onlinecalculator/calculator/calctypes/arithmatic.java
@@ -0,0 +1,24 @@
+package com.onlinecalculator.calculator.calctypes;
+import java.util.*;
+
+
+public class arithmatic {
+public static void main(String args[])
+{
+	int a,b;
+	Scanner s=new Scanner(System.in);
+	System.out.println("enter first number");
+	a=s.nextInt();
+	System.out.println("enter second number");
+	b=s.nextInt();
+	System.out.println("addtion"+(a+b));
+	System.out.println("subtration"+(a-b));
+	System.out.println("multiplication"+(a*b));
+	System.out.println("division"+(a/b));
+	System.out.println("modules"+(a%b));
+	
+	
+		
+	}
+}
+
